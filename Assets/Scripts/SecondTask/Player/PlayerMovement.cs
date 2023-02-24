5ee5c56c565de80ee6825a3a0e60272ba9f8431a
LargonG <pan300503@gmail.com>
2023-02-24 20:27:31 +0300

    Added basic animation
    * Added basic animation for player

--- Assets/Scripts/SecondTask/Player/PlayerMovement.cs
+++ Assets/Scripts/SecondTask/Player/PlayerMovement.cs
@@ -1,13 +1,18 @@
 using System;
+using UnityEditor.Animations;
 using UnityEngine;
 using UnityEngine.Serialization;
 
 namespace SecondTask.Player
 {
-    [RequireComponent(typeof(Rigidbody2D))]
+    [RequireComponent(typeof(Rigidbody2D)), RequireComponent(typeof(Animator))]
     public class PlayerMovement: MonoBehaviour
     {
+        private static readonly int HorizontalMovement = Animator.StringToHash("HorizontalMovement");
+
+        private Transform _transform;
         private Rigidbody2D _rigidbody;
+        private Animator _animator;
 
         [SerializeField] private KeyCode leftKey;
         [SerializeField] private KeyCode rightKey;
@@ -19,11 +24,14 @@ namespace SecondTask.Player
         private bool _jumped;
         
         private readonly Vector2[] _velocities = {Vector2.zero, Vector2.zero};
+        
         private Vector2 _velocity => _velocities[0];
 
         private void Start()
         {
+            _transform = GetComponent<Transform>();
             _rigidbody = GetComponent<Rigidbody2D>();
+            _animator = GetComponent<Animator>();
         }
 
         private void Update()
@@ -62,6 +70,18 @@ namespace SecondTask.Player
         {
             var y = _rigidbody.velocity.y;
             _rigidbody.velocity = new Vector2(direction * speed, y);
+            
+            var rotation = _transform.localRotation;
+            rotation.y = direction switch
+            {
+                < 0 => 180f,
+                > 0 => 0f,
+                _ => rotation.y
+            };
+            
+            _transform.localRotation = rotation;
+            
+            _animator.SetBool(HorizontalMovement, direction != 0);
         }
 
         private void Jump()
